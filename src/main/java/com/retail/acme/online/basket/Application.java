928fd91592d876e398c8b654fb4dc86aae11dfff
HariRangarajan-Solace <HariRangarajan@solace.com>
2023-12-07 11:24:33 +0100

    Initial commit

---
+++ src/main/java/com/retail/acme/online/basket/Application.java
@@ -0,0 +1,56 @@
+package com.retail.acme.online.basket;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.math.BigDecimal;
+
+@SpringBootApplication
+@EnableScheduling
+@Slf4j
+public class Application {
+
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+	@Autowired
+	private StreamBridge streamBridge;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
+
+	@Scheduled(fixedRate = 5000)
+	public void scheduledBasketEvent() {
+		Basket basketTobePosted = createBasketModel();
+		logger.info("Posting Basket event :{}", basketTobePosted);
+		sendRetailAcmeOnlineBasketVerbV1Id(basketTobePosted, "CREATED", "11111");
+	}
+
+	private Basket createBasketModel() {
+		Basket basket = new Basket();
+		basket.setId("11111");
+		basket.setPrice(new BigDecimal(1111));
+		basket.setProduct("Jumper");
+		basket.setQuantity(10);
+		basket.setState("PAID");
+		basket.setCustomerId(111);
+		return basket;
+	}
+	public void sendRetailAcmeOnlineBasketVerbV1Id(
+		Basket payload, String verb, String id
+		) {
+		String topic = String.format("retail/acme/online/basket/%s/v1/%s",
+			verb, id);
+		streamBridge.send(topic, payload);
+	}
+}
